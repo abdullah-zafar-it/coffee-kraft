15cb7f79c6ec0414ba8bc92826da51212b941545
Abdullah Zafar <sardarabdullah498@gmail.com>
2026-08-15 17:36:07 +0500

    Serve static files and css

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '.')));
+app.use('/css', express.static(path.join(__dirname, 'css')));
 
 // API Routes
 app.use('/api/orders', orderRoutes);
